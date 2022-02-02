9a1f347baeb04faa098b7786300b3bb178db3322
DarioCosta <64083309+DarioCostaAlves@users.noreply.github.com>
2022-02-02 23:52:37 +0000

    Starting with Flask. Input and selects.
    Validation with flask.

---
+++ app.py
@@ -0,0 +1,25 @@
+from crypt import methods
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+SPORTS = [
+    "Surf",
+    "Football",
+    "Calisthenics",
+    "Scuba Diving"
+]
+
+@app.route("/")
+def index():
+    return render_template("index.html", sports=SPORTS)
+
+@app.route("/register", methods=["POST"])
+def register():
+    #Validating submission
+    if not request.form.get("name") or request.form.get("sport") not in ["Surf", "Football", "Calisthenics", "Scuba Diving"]:
+        return render_template("failure.html")
+
+    #Confirm registration
+    return render_template("success.html")
+    
