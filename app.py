390ba6732f010ce079bffc7ab0d1fd64aac2afb5
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-02-03 00:26:39 +0000

    Display registrators in memory

--- app.py
+++ app.py
@@ -1,26 +1,45 @@
-from crypt import methods
-from flask import Flask, render_template, request
+# Implements a registration form, storing registrants in a dictionary, with error messages
+
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
+REGISTRANTS = {}
+
 SPORTS = [
-    "Surf",
-    "Football",
-    "Calisthenics",
-    "Scuba Diving",
-    "Golf"
+    "Basketball",
+    "Soccer",
+    "Ultimate Frisbee"
 ]
+
 
 @app.route("/")
 def index():
     return render_template("index.html", sports=SPORTS)
 
+
 @app.route("/register", methods=["POST"])
 def register():
-    #Validating submission
-    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
-        return render_template("failure.html")
 
-    #Confirm registration
-    return render_template("success.html")
-    
+    # Validate name
+    name = request.form.get("name")
+    if not name:
+        return render_template("error.html", message="Missing name")
+
+    # Validate sport
+    sport = request.form.get("sport")
+    if not sport:
+        return render_template("error.html", message="Missing sport")
+    if sport not in SPORTS:
+        return render_template("error.html", message="Invalid sport")
+
+    # Remember registrant
+    REGISTRANTS[name] = sport
+
+    # Confirm registration
+    return redirect("/registrants")
+
+
+@app.route("/registrants")
+def registrants():
+    return render_template("registrants.html", registrants=REGISTRANTS)
